badaa235c187840f45597e8b15e54e0bc998cfd4
sacha Bittner <sacha.bittner@epita.fr>
2025-01-29 15:23:13 +0100

    Changes some names and fixes some small issues from setup and adds a page hello to test link with frontend

--- src/main/java/com/winter/myfirstapp/MyFirstAppApplication.java
+++ src/main/java/com/winter/myfirstapp/MyFirstAppApplication.java
@@ -1,4 +1,4 @@
-package com.winter.myfirstapp.myfirstapp;
+package com.winter.myfirstapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
